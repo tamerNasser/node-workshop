ae2405c4b8ad2ee8ae84e342d22ec95b171a144c
karam1ashqar <karam.ashqar01@gmail.com>
2019-03-24 15:30:45 +0200

    step4, added reader for index.html

--- src/server.js
+++ src/server.js
@@ -1,15 +1,24 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
+let message = "I am so happy to be part of the Node not Girls workshop!";
 
-let message = 'I am so happy to be part of the Node not Girls workshop!';
+const handler = (request, response) => {
+  const url = request.url;
 
-function handler(request, response) {
-  response.writeHead(200, {
-    "Content-Type": "text/html"
-  });
-  response.write(message); //response body
-  response.end(); // finish response
-}
+  if (url === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+
+    fs.readFile(__dirname + "/public/index.html", (error, file) => {
+      if (error) {
+        response.writeHead(500);
+        response.end("500 error not found");
+      } else {
+        response.end(file);
+      }
+    });
+  }
+};
 
 const server = http.createServer(handler);
 
